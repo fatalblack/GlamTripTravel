917f058e1a8a8b1a5e14d34b25ed65e1d14c8da3
Luis Fernando Aguirre <luis.aguirre@plataforma10.com>
2023-11-17 19:50:16 -0300

    corrección en título

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,7 +2,7 @@ const AboutUs = () => {
   return (
     <div id="AboutUs" className="gtt-section-container">
         <div className="gtt-section-title">
-            Nosotros / Sobre m&iacute;
+            Sobre nosotros
         </div>
     </div>
   );
